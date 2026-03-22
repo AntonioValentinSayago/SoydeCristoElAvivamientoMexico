15dbb9f37042686ef09a9f01a4c29228e6795b93
AntonioValentinSayago <cesar_anto30@hotmail.com>
2026-03-21 21:41:39 -0600

    feat(shirts): integrate API-driven

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,7 @@ app.use(morgan('dev'))
 
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://soydecristoelavivamientomexico.onrender.com/api/v1/shirt",
     credentials: true,
 }));
 
