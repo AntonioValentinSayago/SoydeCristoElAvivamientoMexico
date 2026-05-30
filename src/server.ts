9ceb72c50907f101fc885e3d5144a1ac4f3b05e5
AntonioValentinSayago <cesar_anto30@hotmail.com>
2026-05-30 12:03:26 -0600

    fix(Server): fix data rendering and setup TanStack Query client

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from 'express'
 import colors from 'colors'
 import morgan from 'morgan'
 import { db } from './config/db'
-// import QuestionsRouter from './routes/QuestionsRouter'
-// import ShirtRouter from './routes/ShirtRouter'
 import RegisterRouter from './routes/RegisterRouter'
 import MemberRouter from './routes/MemberRouter'
 import cors from 'cors';
@@ -25,15 +23,23 @@ app.use(morgan('dev'))
 app.use(express.json())
 
 const allowedOrigins = [
+    "https://ebenezerprincipedepaz-chalco-mx.vercel.app", 
     "https://ebenezer-soyde-cristo-mexico2026.vercel.app",
-    "http://localhost:5173",
+    "http://localhost:5173"
 ];
+
 app.use(cors({
-    origin: "https://ebenezerprincipedepaz-chalco-mx.vercel.app/",
-    //origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por CORS'));
+        }
+    },
     credentials: true,
 }));
-
 
 app.use('/api/v1/register', RegisterRouter)
 
